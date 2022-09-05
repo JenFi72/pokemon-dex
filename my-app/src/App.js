858af9426e9b554991b9f0809704eeb2255c61d1
Jenfi72 <jennyfiskaare@Jennys-MacBook-Air.local>
2022-09-05 16:35:49 +0200

    axios fetch of pokemons

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
 import Pokedex from './Pokedex';
+import axios from 'axios';
 
 
 function App() {
-  const [pokemon, setPokemon] = useState(["bulbasuar", "charmander"])
-  return (
+  const [pokemon, setPokemon] = useState([])
+
+  axios.get("https://pokeapi.co/api/v2/pokemon")
+.then(res => {
+  setPokemon(res.data.results.map(p => p.name))
+})
+return (
 <Pokedex pokemon={pokemon}/>
   );
 }
