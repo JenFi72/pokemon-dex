c2de1b497a43197bebcc3c73944a9c75bb21327a
Jenfi72 <jennyfiskaare@Jennys-MacBook-Air.local>
2022-09-06 14:14:46 +0200

    added pokemon component + map function

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,42 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+/* import axios from 'axios'; */
 
 
 function App() {
-  const [pokemon, setPokemon] = useState([])
-const [loadMore, setLoadMore] = useState("https://pokeapi.co/api/v2/pokemon?limit=20")
 
-const getPokemons = async () => {
-  const res = await fetch(loadMore)
-  const data = await res.json()
+  const [pokemons, setPokemons] = useState([])
+  const [loadMore, setLoadMore] =useState ("https://pokeapi.co/api/v2/pokemon?limit=20")
 
-  console.log(data)
-}
+  const getPokemons = async () => {
+    const res = await fetch(loadMore)
+    const data = await res.json()
 
-useEffect(() => {
-  getPokemons()
-}, [])
+    setLoadMore(data.next)
+    
+    function singlePokemon (result) {
+      result.forEach( async (pokemon) => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+        const data = await res.json()
+
+        setPokemons(currentArray => [...currentArray, data])
+        
+      })
+
+    }
+    singlePokemon(data.results)
+    await console.log(pokemons)
+  }
+
+  useEffect(() => {
+    getPokemons()
+  }, [])
 
 
 return (
   <div className="pokeBox">
     <div>
     <h1>Pokemon</h1>
+    { pokemons.map(pokemon => <li>{pokemon.name}</li>)}
     <button className="load-more">Loade more</button>
     </div>
   </div>
--- my-app/src/Pokemon.js
+++ my-app/src/Pokemon.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-export default function Pokemon ({ goToNextPage, goToPrevPage}){
+function Pokemon() {
     return (
-        <div>
-            
+        <div className="pokemon">
+            <div className="number">
+<small>#0{id}</small>
+            </div>
+            <img src={image} alt={name} />
+            <div className="details">
+                <h3>{name}</h3>
+                <small>Type: {type}</small>
+            </div>
         </div>
     )
 }
+
+export default Pokemon
